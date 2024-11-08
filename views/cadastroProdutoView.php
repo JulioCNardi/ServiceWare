9d0bc68c4440fea0081af8d6238fc24be8d9b04d
Julio Cesar Nardi <407472@unoesc.lan>
2024-11-07 21:44:03 -0300

    ajustado para criacao do crud

---
+++ views/cadastroProdutoView.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>ServiceWare</title>
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="../css/estilo.css">
+
+    <!-- Font Awesome JS -->
+    <script src="https://kit.fontawesome.com/ae360af17e.js" crossorigin="anonymous"></script>
+</head>
+<body class="container-fluid p-0 d-flex h-100">
+
+    <!-- Menu lateral -->
+    <div class="wrapper">     
+        <!-- Main Component -->
+        <div class="main">
+            <nav class="navbar navbar-expand px-3">
+                <!-- Button for sidebar toggle -->
+                <button class="btn" type="button btn-dark" data-bs-theme="dark">
+                    <a href="../index.php">Voltar</a>
+                </button>
+            </nav>
+            <main class="content px-3 ">
+                <div class="container-fluid">
+                <div class="mb-3 text-center">
+                </div>
+                </div>
+            </main>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
+    </header>
+    
+</body>
